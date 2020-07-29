030114aa6315fd8c6d14669dd9cc4e6bf829bd5c
Faizy51 <faizulla49@yahoo.co.uk>
2020-07-29 23:46:13 +0530

    UI setup for phone auth.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatelessWidget {
+  final phoneNumberController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,81 +14,142 @@ class AuthScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            // crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Image.asset(
                 'assets/images/logo.png',
-                scale: 1.3,
-                ),
-              Padding(
-              padding: EdgeInsets.fromLTRB(50, 50, 50, 20),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  icon: Icon(
-                    Icons.phone_android,
-                  ),
-                  hintText: "Phone"
-                ),
+                scale: 0.9,
               ),
+              SizedBox(
+                height: 100,
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50),
+                padding: EdgeInsets.fromLTRB(50, 0, 50, 20),
                 child: TextField(
-                  scrollPadding: EdgeInsets.symmetric(horizontal: 50),
-                  obscureText: true,
+                  controller: phoneNumberController,
+                  keyboardType: TextInputType.number,
                   decoration: InputDecoration(
-                    icon: Icon(
-                      Icons.lock
-                    ),
-                    hintText: "Password"
+                      icon: Icon(
+                        Icons.phone_android,
+                      ),
+                      hintText: "Enter your Phone number"),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(40),
+                child: FlatButton(
+                  onPressed: () {
+                    // todo: Make firebase request and show alert.
+                    FocusScope.of(context).unfocus();
+                    String phoneNumber = phoneNumberController.text;
+                    if (phoneNumber.isEmpty || phoneNumber.length < 10) {
+                      _showAlertDialog(context);
+                    } else {
+                      _showOTPDialog(context);
+                    }
+                  },
+                  padding: EdgeInsets.symmetric(horizontal: 100),
+                  color: Colors.deepOrange[900],
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
               ),
               SizedBox(
-                
-              ),
-              FlatButton(
-                onPressed: () {}, 
-                child: Text(
-                  "Forgot Password?",
-                  style: TextStyle(letterSpacing: 2, fontSize: 17),
-                  )
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(40),
-                  child: FlatButton(
-                    onPressed: () {}, 
-                    padding: EdgeInsets.symmetric(horizontal: 100),
-                    color: Colors.deepOrange[900],
-                    child: Text(
-                      "Login",
-                      style: TextStyle(color: Colors.white70),
-                    ),
-                  ),
-                ),
-                Text(
-                  "or"
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(40),
-                  child: FlatButton(
-                    onPressed: () {}, 
-                    padding: EdgeInsets.symmetric(horizontal: 93),
-                    color: Colors.deepOrange[900],
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(color: Colors.white70),
-                    ),
-                  ),
-                ),
+                height: 90,
+              )
             ],
           ),
         ),
       ),
+    );
+  }
+
+
+
+  // Alerts
+  Future<void> _showAlertDialog(BuildContext context) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: true,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+        title: Text('Invalid Phone number!'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('The phone number you entered is empty, or not valid. Please enter it again.'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            color: Colors.blue,
+            child: Text('I understand'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+  Future<void> _showOTPDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          title: Text('Please enter the OTP here.'),
+          backgroundColor: Colors.lightBlue[50],
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('An OTP is sent to your mobile number.'),
+                Text('Enter it if not auto-detected.'),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.fiber_pin),
+                    hintText: 'Enter code here.',
+                  ),
+                  validator: (String value) {
+                    return value.contains('@')
+                        ? 'Do not use the @ char.'
+                        : null;
+                  },
+                )
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              color: Colors.red[700],
+              highlightColor: Colors.white,
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              color: Colors.blue,
+              child: Text('Done'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
