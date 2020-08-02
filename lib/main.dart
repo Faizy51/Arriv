c08cc68b5b941d906c1696073ef8a30222a7b366
Faizy51 <faizulla49@yahoo.co.uk>
2020-08-02 21:31:51 +0530

    Auth complete. Along with appropriate screen to show w.r.t user session

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:Arriv/home_screen.dart';
 import 'package:Arriv/login_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -7,27 +8,61 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  Future _userFuture;
+  FirebaseUser user;
+  var regUser;
+
+  @override
+  void initState() {
+    super.initState();
+    // Move this to a method, maybe?
+    _userFuture = FirebaseAuth.instance.currentUser().then((value) async {
+      user = value;
+      print('Printing user after future1: $user');
+      await Firestore.instance.collection("users").document(user.uid).get().then((value) {
+        regUser = value.data;
+        print("everything complete: $regUser");
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<FirebaseUser>(
-      future: FirebaseAuth.instance.currentUser(),
-      builder: (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
-        if (snapshot.hasData) {
-          FirebaseUser user = snapshot.data; // this is your user instance
-          /// The user is already logged In.
-          return buildMaterialApp(HomeScreen(user));
+    return FutureBuilder(
+      future: _userFuture,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (regUser == null) {
+            // The user hasn't signed up.
+            return buildMaterialApp(AuthScreen());
+          }
+          else {
+            return buildMaterialApp(HomeScreen(user));
+          }
         }
-        /// The user is not logged In.
-        return buildMaterialApp(AuthScreen());
+        
+        if (user != null) {
+          return buildMaterialApp(HomeScreen(user));
+        } else {
+          return buildMaterialApp(Scaffold(
+          body: Center(child: CircularProgressIndicator())
+        ));
+        }
       },
     );
   }
 
   MaterialApp buildMaterialApp(Widget screen) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo', // Why is this so?
       theme: ThemeData(
         primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
